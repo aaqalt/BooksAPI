997b51a576767bf36d267aa245498246df71fb03
Bilol-ui <yolchixojayevbilol6@gmail.com>
2025-08-21 17:08:22 +0500

    fixed

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import ForeignKey
+
 
 class User(AbstractUser):
     ROLE_CHOICES = (
@@ -9,8 +11,6 @@ class User(AbstractUser):
     phone = models.CharField(max_length=15, unique=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES,default="member")
 
-class Book(models.Model):
-    pass
 
 class Borrow(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,3 +20,22 @@ class Borrow(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField
+    published_date = models.DateTimeField(null=True,blank=True)
+    category = ForeignKey(Category,on_delete=models.CASCADE, related_name="books")
+    available_copies = models.PositiveIntegerField(default=1)
+    total_copies = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
